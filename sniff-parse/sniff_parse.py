df85cacce8e2e65ee69546f2e0ba6f1168c2ea82
root <root@budoheiwa.budoheiwa>
2024-03-02 16:54:09 +0100

    Update sniff_parse script, for filtering data

--- sniff-parse/sniff_parse.py
+++ sniff-parse/sniff_parse.py
@@ -11,8 +11,32 @@ def parse_pcap(capture_file):
 
     for packet in packets:
         print(packet.summary())
+        
         if Raw in packet:
-            raw_data = packet[Raw].load
+            raw_data = packet[Raw].load.decode('utf-8')
+
+            if raw_data.startswith('USER'):
+                username = raw_data.split(' ')[1].strip()
+                raw_data_list.append(f'Username: {username}')
+
+            elif raw_data.startswith('PASS'):
+                password = raw_data.split(' ')[1].strip()
+                raw_data_list.append(f'Password: {password}')
+
+            elif raw_data.startswith('STOR'):
+                filename = raw_data.split(' ')[1].strip()
+                raw_data_list.append(f'File: {filename}')
+                raw_data_list.append('---Start of File Content---')
+
+                for next_packet in packets[packets.index(packet) + 1:]:
+                    if Raw in next_packet:
+                        next_raw_data = next_packet[Raw].load.decode('utf-8')
+                        if next_raw_data.startswith('STOR'):
+                            break
+                        else:
+                            raw_data_list.append(next_raw_data)
+                raw_data_list.append('---End of File Content---')
+
 #            print(f"Raw Data: {raw_data}")
             format_raw = f"Raw Data: {raw_data}"+"\n"
             raw_data_list.append(format_raw)
@@ -21,8 +45,9 @@ def parse_pcap(capture_file):
 
 def exifiltr_data(raw_data_list):
     with open("FTP_rawdata.txt", "w") as w:
-        for packet in raw_data_list:
-            w.write(str(packet))
+        for data in raw_data_list:
+            cleaned_data = data.strip("b'\n")
+            w.write(cleaned_data + '\n')
 
 def read_data():
     with open("FTP_rawdata.txt", "r") as r:
