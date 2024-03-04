81ab472a8a78bbe7a87507b1158cd2a830ad1569
root <root@budoheiwa.budoheiwa>
2024-03-04 12:27:42 +0100

    Update sniff_parse.py, failed to sniff packets after adding new network interface for FTPS server. Conflict with FTP raw data packets filtering

--- sniff-parse/sniff_parse.py
+++ sniff-parse/sniff_parse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from scapy.all import *
 
-interfaces = ["eth0", "lo", "br-5553900d8119", ""]
-capture = sniff(iface=interfaces, filter="port 21 or (portrange 21100-21110) or port 22 or (portrange 30000-30009)", count=90)
+interfaces = ["eth0", "lo", "br-5553900d8119", "br-fb9de0832914"]
+capture = sniff(iface=interfaces, filter="port 21 or (portrange 21100-21110) or (portrange 30000-30009)", count=90)
 wrpcap("capturestocker.pcap", capture)
 
 def parse_pcap(capture_file):
