51d8faf8a348b6e6107b9018233b0316e3b0be76
root <root@budoheiwa.budoheiwa>
2024-03-05 16:48:45 +0100

    All is up-to-date

---
+++ ftp_auto.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from scapy.all import *
+import subprocess
+import os
+    
+def start_ftp_server(command):
+    try:
+        subprocess.run(command, check=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+
+def exec_sniff_parse():
+    subprocess.run(["sudo", "python3", "sniff-parse/sniff_parse_FTP.py"])
+
+if __name__ == "__main__":
+    print("Code 1: Starting FTP server")
+    command = "sudo docker restart ftp-server"
+    start_ftp_server(command)
+    print("\r"+"#" * 50 + "\n")
+    
+    print("Code 2: Sniffing any FTP packets with parse")
+    exec_sniff_parse()
+    print("#" * 50 + "\n")
+    
+    print("\r"+"Code 3: Uploading files to FTP server")
+    print("Files uploaded successfully.")
+    print("\r"+"#" * 50 + "\n")
+    
+   
+    
+   
+    
+    
