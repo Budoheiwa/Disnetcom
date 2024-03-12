5434baec24b4d09fdf8666f12ad3d1f8208cc761
root <root@budoheiwa.budoheiwa>
2024-03-13 00:22:35 +0100

    Creation of all_auto.py that can automate all python scripts

---
+++ all_auto.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+import subprocess
+import os
+    
+def exec_ftp_auto():
+    subprocess.run(["sudo", "python3", "ftp_auto.py"])
+
+def exec_ftps_auto():
+    subprocess.run(["sudo", "python3", "ftps_auto.py"])
+
+if __name__ == "__main__":
+    print("--Executing ftp_auto.py script--")
+    exec_ftp_auto()
+    print("\r"+"#" * 50 + "\n")
+    
+    print("--Executing ftps_auto.py script--")
+    exec_ftps_auto()
+    print("\r"+"#" * 50 + "\n")
+  
+       
+   
+    
+   
+    
+    
