19117b237ce9f1fd40acca6f912a293eb325893c
budoheiwa <voan.ngpro@gmail.com>
2024-03-14 16:02:25 +0100

    Update sniff_parse_FTPS.py - From count to time functions
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- sniff-parse/sniff_parse_FTPS.py
+++ sniff-parse/sniff_parse_FTPS.py
@@ -6,13 +6,18 @@ import time
 def intercept_packets(interface, filter_expression, timeout):
     start_time = time.time()
     packets = []
+    first_packet_received = False
     
-    print(f"Intercepting packets on interface {interface} for {timeout} seconds...")
+    print(f"Intercepting packets on interface {interface}...")
     while time.time() - start_time < timeout:
         timeout_func = timeout - (time.time() - start_time)
         captured_packets = sniff(iface=interface, filter=filter_expression, timeout=timeout_func)
         if captured_packets:
             packets += captured_packets
+            if not captured_packets:
+                print("First packet intercepted. Starting timer...")
+                start_time = time.time()  # Reset the start time
+                first_packet_received = False
         else:
             break
     
@@ -45,9 +50,9 @@ def pcap_filter_packets(capture_file, filtered_pcap_file):
         print(f"Error executing command: {e}")
 
 if __name__ == "__main__":
-    interface = ["eth0", "lo", "br-508d6f4743bc"]
+    interface = ["eth0", "lo", "br-508d6f4743bc"]  
     filter_expression = "port 21 or (portrange 30000-30010)"
-    timeout = 20  # Seconds
+    timeout = 15  # Seconds
 
     captured_packets = intercept_packets(interface, filter_expression, timeout)
     if captured_packets:
