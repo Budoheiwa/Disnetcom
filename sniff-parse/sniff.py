9321d60087c8b996995b0574271970d68ae51d2f
budoheiwa <voan.ngpro@gmail.com>
2024-03-15 19:51:18 +0100

    Up-to-date sniff.py - parse_pcap.py - sniff_parse_FTPS.py scripts
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- sniff-parse/sniff.py
+++ sniff-parse/sniff.py
@@ -2,6 +2,6 @@
 from scapy.all import *
 
 
-interfaces = ["eth0", "lo", "br-2c6ea089bf18"] #add new network interfaces
+interfaces = ["eth0", "lo", "br-c5a509f486c7"] #add new network interfaces
 capture = sniff (iface = interfaces, filter = "port 21 or portrange(21100-21110)", count = 150) #add or change filter and count
 wrpcap("capturestocker.pcap", capture)
