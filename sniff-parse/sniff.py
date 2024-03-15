f7bfe827e2ed0280ab6de504d2ccfe9757fc1d03
budoheiwa <voan.ngpro@gmail.com>
2024-03-15 10:25:22 +0100

    Update sniff.py script - adding some comments
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- sniff-parse/sniff.py
+++ sniff-parse/sniff.py
@@ -2,6 +2,6 @@
 from scapy.all import *
 
 
-interfaces = ["eth0", "lo", "br-2c6ea089bf18"]
-capture = sniff (iface = interfaces, filter = "port 21 or portrange(21100-21110)", count = 150)
+interfaces = ["eth0", "lo", "br-2c6ea089bf18"] #add new network interfaces
+capture = sniff (iface = interfaces, filter = "port 21 or portrange(21100-21110)", count = 150) #add or change filter and count
 wrpcap("capturestocker.pcap", capture)
