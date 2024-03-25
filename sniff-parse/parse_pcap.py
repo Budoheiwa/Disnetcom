a7c7f3f289f959ea6ae62b23fe1f4c31bd8c8ca2
budoheiwa <voan.ngpro@gmail.com>
2024-03-25 16:57:11 +0100

    Update parse_pcap.py
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- sniff-parse/parse_pcap.py
+++ sniff-parse/parse_pcap.py
@@ -2,10 +2,10 @@
 from scapy.all import *
 
 def parse_pcap(capture):
-	packets = rdpcap(capture) #Loading and reading the pcap file
+	packets = rdpcap(capture) # Loading and reading the pcap file
 	
 	for packet in packets:
-		print(packet.summary()) #Show the summary for each packet
+		print(packet.summary()) # Show the summary for each packet
 
-capture = "capturestocker.pcap"
+capture = "capturestocker.pcap" # Select your pcap file
 parse_pcap(capture)
