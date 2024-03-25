b41c8575a4c91a86fbcd4d280a24e4675b568740
budoheiwa <voan.ngpro@gmail.com>
2024-03-25 16:55:54 +0100

    Update sniff.py
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- sniff-parse/sniff.py
+++ sniff-parse/sniff.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from scapy.all import *
 
-
-interfaces = ["eth0", "lo", "br-c5a509f486c7"] #add new network interfaces
-capture = sniff (iface = interfaces, filter = "port 21 or portrange(21100-21110)", count = 150) #add or change filter and count
-wrpcap("capturestocker.pcap", capture)
+interfaces = ["eth0", "lo", "br-c5a509f486c7"] # add new network interfaces
+capture = sniff (iface = interfaces, filter = "port 21 or portrange(21100-21110)", count = 150) # add or change filter and count
+wrpcap("capturestocker.pcap", capture) # pcap file is stored
