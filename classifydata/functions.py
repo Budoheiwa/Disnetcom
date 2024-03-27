74038611882f51587cb9b6cad858652e6068bea3
budoheiwa <voan.ngpro@gmail.com>
2024-03-27 01:13:26 +0100

    Update classifydata folder - Adding new scripts and some modifications were made !
    Signed-off-by: budoheiwa <voan.ngpro@gmail.com>

--- classifydata/functions.py
+++ classifydata/functions.py
@@ -7,13 +7,17 @@ def copy_lines(input_file_name, output_file_name, keywords):
             line = lines[i]
             for word in keywords:
                 if word in line:
+                    next_line = line
+                    output_file.write(word + '\n')  # Write the keyword
+                    output_file.write(next_line)  # Write the next line
+"""                    
                     if i+2 < len(lines):  # Ensure there are next two lines available
                         next_line1 = lines[i+1]
                         next_line2 = lines[i+2]
                         output_file.write(word + '\n')  # Write the keyword
                         output_file.write(next_line1)  # Write the next line
                         output_file.write(next_line2)  # Write the next line
-
+"""
 def create_keygroups(inputkeyfile):
     keyword_group = []
     with open(inputkeyfile, 'r') as inputkey:
--- classifydata/main.py
+++ classifydata/main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import functions
+from functions import *
 
-# Call create_keygroups to get the list of keywords
-keyword_group = create_keygroups("users.txt") # text file will be our dictionnary
+if __name__ == "__main__":
+    # Call create_keygroups to get the list of keywords
+    keyword_group = create_keygroups("users.txt") # text file will be our dictionnary
 
-# Now call copy_lines with the obtained keyword_group
-input_file_name = 'D:/dossier/Cours/INGE3/projet/ftp_rawdata.txt' # Path folder of the input file
-output_file_name = 'D:/dossier/Cours/INGE3/projet/mdp.txt' # Path folder of the output file
+    # Now call copy_lines with the obtained keyword_group
+    input_file_name = './testread.txt' # Path folder of the input file
+    output_file_name = './credentials.txt' # Path folder of the output file
 
-copy_lines(input_file_name, output_file_name, keyword_group)
+    copy_lines(input_file_name, output_file_name, keyword_group)
 
+# Need to create dictionnaries for all types of secret information
